79511d6a5d00042b71a661b36f08ff5b86def924
Keely Brennan <keelybrennan@Keelys-MacBook-Pro-2.local>
2018-04-13 21:59:41 -0500

    switch case for command line functions and omdb function

--- liri.js
+++ liri.js
@@ -1,45 +1,96 @@
-// read and set any environment variables with the dotenv package
+//read and set the environment variables with the dotenev package: 
 require("dotenv").config();
 
-//import the keys.js file and store it in a variable 
-var spotify = new Spotify(keys.spotify);
-var client = new Twitter(keys.twitter);
+var keys = require("./keys.js");
+var fs = require("fs");
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
-//request npm package
-var request = require("request");
 
-//store all arguments in an array
-var nodeArgs = process.argv;
-var movieName = "";
+//variable to feed to randomTxtFunc
+var randomtxt;
 
-//for movie names that are greater than one word
-for (var i=2; i < nodeArgs.length; i++) {
-    if i>2 && i < nodeArgs.length) {
-        moveiName = movieName + "+" + nodeArgs[i];
-    }
-    else {
-        movieName += nodeArgs[i];
-    }
-}
-//run request to OMDB
-var queryURL = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+//variable to store user input from command line
+var userInput = process.argv[2];
 
-//debug 
-console.log(queryURL);
-
-request(queryURL, function(error, response, body) {
-
-  // if request is successful
-  if (!error && response.statusCode === 200) {
-    console.log("Title: " + JSON.parse(body).Title);
-    console.log("Release Year: " + JSON.parse(body).Year);
+//switch case for 4 command line arguments
+switch (userInput) {
+  case "my-tweets":
+    twitterFunc();
+    break;
+  case "spotify-this-song":
+    spotifyFunc();
+    break;
+  case "movie-this":
+    movieFunc();
+    break;
+  case "do-what-it-says":
+    randomTxtFunc();
+    break;
 }
 
+//function for my-tweets
+//function twitterFunc(){
+  //var client = new Twitter(keys.twitter);
 
-//10. Make it so liri.js can take in one of the following commands:
-//if process.argv[2]
+//}
 
-//my-tweets
-//spotify-this-song
-//movie-this
-//do-what-it-says
+
+//function for spotify-this-song
+//function spotifyFunc(){
+  //var spotify = new Spotify(keys.spotify);
+//}
+
+
+
+//function for movie-this
+function movieFunc(){
+  var request = require("request");
+  var movie = randomtxt;
+
+  if (movie == null){
+    movie = process.argv[3];
+
+    //if movie name is more than one word 
+    for (var i = 4; i < process.argv.length; i++) {
+      if (i > 4 && i < process.argv.length) {
+        movie = movie + " " + process.argv[i];
+      }else {
+        movie = process.argv[3];
+      }
+    }
+      if (movie == null){
+        console.log("Not sure what to watch? How about Mr. Nobody!")
+        movie = "Mr. Nobody";
+      }
+
+  // run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+
+    request(queryUrl, function(error, response, body){
+
+      //if request is successful 
+      if (!error && response.statusCode === 200) {
+        console.log("");
+        //parse body of the site and return information
+        console.log("Title: " + JSON.parse(body).Title);
+        console.log("Release Year: " + JSON.parse(body).Year);
+        console.log("IMDB Rating of the movie: " + JSON.parse(body).Ratings[0].Value);
+        console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+        console.log("Country where movie was produced: " + JSON.parse(body).Production);
+        console.log("Language: " + JSON.parse(body).Language);
+        console.log("Plot: " + JSON.parse(body).Plot);
+        console.log("Actors: " + JSON.parse(body).Actors);
+      }else {
+        console.log(error);
+      }
+      
+    });
+    
+  }
+
+  //function for do-what-it-says 
+//function randomTxtFunc(){
+//}
+};
